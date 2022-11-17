2fb8567b4e1e5b853ad8e9fe2a2709261fbde26e
nathalie <nathalie@fitlab.fr>
2022-11-17 11:38:14 +0100

    change la couleur du rdv wip

--- app/controllers/admin/user_in_waiting_rooms_controller.rb
+++ app/controllers/admin/user_in_waiting_rooms_controller.rb
@@ -15,6 +15,23 @@ class Admin::UserInWaitingRoomsController < AgentAuthController
 
     if current_organisation.territory.enable_waiting_room_color_field
       # change la couleur du rdv
+
+
+
+      puts "*" *100
+      puts "passe par la mofication de la couleur du rdv"
+      puts "*" *100
+
+      require 'redis'
+      redis = Redis.new(host: "localhost")
+
+      redis.set("user_in_waiting_room", rdv)
+
+      puts "#" *100
+      puts redis.get("user_in_waiting_room")
+      puts "#" *100
+
+
     end
   end
 end
